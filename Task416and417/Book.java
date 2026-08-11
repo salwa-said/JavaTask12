1bd1bea6db9659d87b5f73e7f9dd600c7266718a
salwa-said <salwwa2000@gmail.com>
2026-08-11 16:07:53 +0400

    Task416and417

---
+++ Task416and417/Book.java
@@ -0,0 +1,13 @@
+package Task416and417;
+
+public class Book {
+    String title;
+    String author;
+
+    void  printInfo(){
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+    }
+
+
+}
